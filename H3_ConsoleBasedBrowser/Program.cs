e78622d84bae07ada9dcb68b54ed88d421ed48b9
Kenneth Folke Larsen <KennethLarsenBusiness@hotmail.com>
2021-08-09 13:17:05 +0200

    Add project files.

---
+++ H3_ConsoleBasedBrowser/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace H3_ConsoleBasedBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string htmlResponse;
+
+            Task<string> result = GetWebsite("https://en.wikipedia.org/wiki/Eurasian_otter");
+            if (!result.IsFaulted)
+            {
+                htmlResponse = Regex.Replace(result.Result, @"<[^>]*>", "");
+            }
+            else
+                htmlResponse = result.Result;
+
+            Console.WriteLine(htmlResponse);
+            Console.ReadLine();
+
+        }
+
+        static async Task<string> GetWebsite(string url)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage message = await client.GetAsync(url);
+                    message.EnsureSuccessStatusCode();
+                    string response = await message.Content.ReadAsStringAsync();
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
